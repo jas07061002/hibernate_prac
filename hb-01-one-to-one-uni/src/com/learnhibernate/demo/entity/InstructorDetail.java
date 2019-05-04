17d021e0aeb570d3d9d3c0e604ff0bddfb962343
jas07061002 <jas18kalra@gmail.com>
2019-05-04 16:06:49 -0400

    one to one mapping in Hibernate initial commit

---
+++ hb-01-one-to-one-uni/src/com/learnhibernate/demo/entity/InstructorDetail.java
@@ -0,0 +1,17 @@
+package com.learnhibernate.demo.entity;
+
+public class InstructorDetail {
+	
+	// annotate the class as an entity and map it to a db table
+	
+	// define the fields
+	
+	// annotate the fields with db column names
+	
+	// create constructors
+	
+	// generate getter/setter methods
+	
+	// generate toString() method
+
+}
