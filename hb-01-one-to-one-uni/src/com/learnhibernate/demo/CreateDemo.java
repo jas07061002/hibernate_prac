8c3a57c45b0dc231669e493b9bda2337f346dc04
jas07061002 <jas18kalra@gmail.com>
2019-05-04 16:52:38 -0400

    Updated the class for added functionality for one to one hibernate mapping

--- hb-01-one-to-one-uni/src/com/learnhibernate/demo/CreateDemo.java
+++ hb-01-one-to-one-uni/src/com/learnhibernate/demo/CreateDemo.java
@@ -26,12 +26,21 @@ public class CreateDemo {
 		try {
 			
 			// create the objects
+//			Instructor tempInstructor =
+//					new Instructor("Chad", "Darby", "darby@luv2code.com");
+//			
+//			InstructorDetail tempInstructorDetail = 
+//					new InstructorDetail("http://www.luv2code.com/youtube", 
+//							"Luv 2 code!!!");
+//			
 			Instructor tempInstructor =
-					new Instructor("Chad", "Darby", "darby@luv2code.com");
+					new Instructor("Madhu", "Patel", "madhu@luv2code.com");
 			
 			InstructorDetail tempInstructorDetail = 
-					new InstructorDetail("http://www.luv2code.com/youtube", 
-							"Luv 2 code!!!");
+					new InstructorDetail("http://www.youtube.com/", 
+							"Guitar");
+			
+		
 			
 			// associate the objects 
 			tempInstructor.setInstructorDetail(tempInstructorDetail);
