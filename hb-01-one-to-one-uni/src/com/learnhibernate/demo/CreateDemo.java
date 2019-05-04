1aabb47cb89687886baa31511361918c1d89e347
jas07061002 <jas18kalra@gmail.com>
2019-05-04 16:47:48 -0400

    Added one to one mapping on hibernate

--- hb-01-one-to-one-uni/src/com/learnhibernate/demo/CreateDemo.java
+++ hb-01-one-to-one-uni/src/com/learnhibernate/demo/CreateDemo.java
@@ -5,37 +5,47 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import com.learnhibernate.demo.entity.Instructor;
+import com.learnhibernate.demo.entity.InstructorDetail;
 import com.learnhibernate.demo.entity.Student;
 
-public class CreateStudentDemo {
+public class CreateDemo {
 
 	public static void main(String[] args) {
-		
-		
 		
 		// create session factory
 		SessionFactory factory = new Configuration()
 								.configure("hibernate.cfg.xml")
-								.addAnnotatedClass(Student.class)
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
 								.buildSessionFactory();
 		
 		// create a session
 		Session session = factory.getCurrentSession();
 		
 		try {
-			 
-	         
-			// create a student object
-			System.out.println("Creating a new student object...");
-			Student tempStudent = new Student("Paul","Wall","paul@luv2code.com");
+			
+			// create the objects
+			Instructor tempInstructor =
+					new Instructor("Chad", "Darby", "darby@luv2code.com");
+			
+			InstructorDetail tempInstructorDetail = 
+					new InstructorDetail("http://www.luv2code.com/youtube", 
+							"Luv 2 code!!!");
+			
+			// associate the objects 
+			tempInstructor.setInstructorDetail(tempInstructorDetail);
 			
 			// start a transaction
 			session.beginTransaction();
 			
-			// save the student object
-			System.out.println("Saving the student.....");
-			session.save(tempStudent);
+			// save the instructor
 			
+			// Note: This will also save the details object because of CascadeType.ALL
+			System.out.println("Saving instructor: " +tempInstructor);
+			session.save(tempInstructor);
+			
+
 			// commit the transaction
 			session.getTransaction().commit();
 			
